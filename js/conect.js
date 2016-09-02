6f3f20e6df1377429bd35abb30919a770cb0dd6f
digitalizarte <digitalizarte@gmail.com>
2016-09-02 15:16:17 -0300

    Se eliminó el javascript intrusivo del HTML. Se reorganizo el código javascript.

--- js/conect.js
+++ js/conect.js
@@ -1,218 +1,151 @@
 // Hora y fecha 
-$(function() {
-    $('#hora').timepicker();
-});
+$(document).ready(function () {
+  // Inicia los controles.
+  $('#fecha').datepicker();
+  $('#hora').timepicker();
 
-$( document ).ready(function() {
-    $('#fecha').datepicker();
-});
+  // Funciones del parsley para que avanze de instancias
+  $('.previous').on('click', function () {
+    var idx = curIndex();
+    navigateTo(idx - 1);
+  });
 
+  $('.next').on('click', function () {
+    var $demoForm = $('.demo-form-2');
+    var parsley = $demoForm.parsley();
+    var idx = curIndex();
+    var data = {
+      group: 'block-' + String(idx)
+    };
 
+    if (parsley.validate(data)) {
+      navigateTo(idx + 1);
+    }
+  });
 
-//funciones del parsley para que avanze de instancias
-$(function () {
-          var $sections = $('.form-section-2');
-          function navigateTo(index) {
-            $sections
-              .removeClass('current')
-              .eq(index)
-                .addClass('current');
-            $('form-navigation-2 .previous').toggle(index > 0);
-            var atTheEnd = index >= $sections.length - 1;
-            $('.form-navigation-2 .next').toggle(!atTheEnd);
-            $('.form-navigation-2 [type=submit]').toggle(atTheEnd);
-          }
+  // Cambio de cantidad de vehiculos.
+  $('.vehicleQty').on('change', function () {
+    var $this = $(this);
+    if ($this.val() != i18n.QuantityText) {
+      $this.prev().attr('checked', 'checked');
+    }else {
+      $this.prev().removeAttr('checked');
+    }
+  });
 
-          function curIndex() {
-            return $sections.index($sections.filter('.current'));
-          }
-          $('.previous').click(function() {
-            navigateTo(curIndex() - 1);
-          });
-          $('.next').click(function() {
-            if ($('.demo-form-2').parsley().validate({group: 'block-' + curIndex()}))
-              navigateTo(curIndex() + 1);
-          });
-          $sections.each(function(index, section) {
-            $(section).find(':input').attr('data-parsley-group', 'block-' + index);
-          });
-          navigateTo(0);
-});
+  // Al seleccionar un vehiculo activa el primer valor de la cantidad de vehiculos. 
+  // Y si se seleciono una cantidad de vehiculos marca automaticamente el imput del vehiculo.
+  // Selección de un servicio.
+  $('.servicio').on('click', function (e) {
+    if (e.target.className == 'radi' || e.target.nodeName == 'SELECT' || e.target.nodeName == 'OPTION') return // prevent to check/uncheck when click on inner controls
+    var $this = $(this);
+    var $radi =  $this.find('.radi');
+    var $select = $this.find('select');
+    var checked = $radi.prop('checked') || false;
 
+    if (checked) {
+      $radi.prop('checked', false);
+      $select.val(i18n.QuantityText);
+    } else {
+      $radi.prop('checked', true);
+      if ($select.val() == i18n.QuantityText) {
+        $select.val('1');
+      }
+    }
+  }).find(':checkbox').on('click', function () {
+    var $this = $(this);
+    var $combo = $this.next();
+    if ($this.is(':checked')) {
+      if ($combo.val() == i18n.QuantityText) {
+        $combo.val('1');
+      }
+    } else {
+      $combo.val(i18n.QuantityText);
+    }
+  });
+  
+  // Para la galeria de Autos
+  $('#selectChange').on('change', function () {
+    $('#option1, #option2, #option3, #option4, #option5').fadeOut(250);
+    $('#' + $(this).val()).stop().fadeIn(1500);
+  });
 
-
-// Para la galeria de Autos
-function pagoOnChange(sel) {
-        if (sel.value == "option1") {
-            $("#option1").fadeIn(1500);
-            $("#option2").fadeOut(250);
-            $("#option3").fadeOut(250);
-            $("#option4").fadeOut(250);
-            $("#option5").fadeOut(250);
-
-        }
-
-        if (sel.value == "option2") {
-            $("#option2").fadeIn(1500);
-            $("#option1").fadeOut(250);
-            $("#option3").fadeOut(250);
-            $("#option4").fadeOut(250);
-            $("#option5").fadeOut(250);
-
-        }
-
-        if (sel.value == "option3") {
-            $("#option3").fadeIn(1500);
-            $("#option1").fadeOut(250);
-            $("#option2").fadeOut(250);
-            $("#option4").fadeOut(250);
-            $("#option5").fadeOut(250);
-
-        }
-
-        if (sel.value == "option4") {
-            $("#option4").fadeIn(1500);
-            $("#option1").fadeOut(250);
-            $("#option2").fadeOut(250);
-            $("#option3").fadeOut(250);
-            $("#option5").fadeOut(250);
-
-        }
-
-
-        if (sel.value == "option5") {
-            $("#option5").fadeIn(1500);
-            $("#option1").fadeOut(250);
-            $("#option2").fadeOut(250);
-            $("#option3").fadeOut(250);
-            $("#option4").fadeOut(250);
-
-        }
+  // Efecto para los controles input.
+  $('input.input__field').each(function () {
+    var $this = $(this);
+    if ($.trim($this.val()) !== '') {
+      $this.parent().addClass('input--filled');
     }
 
-
-
-
-// al seleccionar un vehiculo activa el primer valor de la cantidad de vehiculos. Y si se seleciono una cantidad de vehiculos marca automaticamente el imput del vehiculo.
-
-    $('.servicio').find(':checkbox').click(function () {
-        var $this = $(this),
-            $combo = $this.next();
-        if ($this.is(":checked")) {
-            if ($combo.val() == i18n.QuantityText) {
-                $combo.val("1");
-            }
-        } else {
-            $combo.val(i18n.QuantityText);
-        }
+    $this.on('focus', function () {
+      var $this = $(this);
+      $this.parent().addClass('input--filled');
+    }).on('blur', function () {
+      var $this = $(this);
+      if ($.trim($this.val()) === '') {
+        $this.parent().removeClass('input--filled');
+      }
     });
+  });
 
-    $('.vehicleQty').change(function () {
-        if ($(this).val() != i18n.QuantityText)
-            $(this).prev().attr("checked", "checked");
-        else
-            $(this).prev().removeAttr("checked");
-    });
+  // autocomplete de direcciones.
+  $('#origen').geocomplete();
 
-    $('.servicio').click(function (e) {
-        if (e.target.className == "radi" || e.target.nodeName == 'SELECT' || e.target.nodeName == 'OPTION') return; // prevent to check/uncheck when click on inner controls
-        var $this = $(this);
-        var checked = $('.radi', $this).prop('checked') || false;
+  $('#destino').geocomplete();
 
-        if (checked) {
-            $('.radi', $this).prop('checked', false);
-            $('select', $this).val(i18n.QuantityText);
-        } else {
-            $('.radi', $this).prop('checked', true);
-            if ($('select', $this).val() == i18n.QuantityText) {
-                $('select', $this).val("1");
-            }
-        }
-    });
+  // para mostrar el select de servicio por hora y ocultarlo si se seleciona por destino
+  $('a div.servipresudes').on('click', mostrarDestino);
+  $('a div.servipresuhor').on('click', mostrarHora);
 
-   
+  $('#enviausuario').on('click', function () {
+    // Envia el nombre del usuario a la instacia 3 y 4 del formulario.
+    var nombre = $('#nombre').val();
+    $('#nombreusuarioa').val(nombre);
+    $('#nombreusuariob').val(nombre);
+  })
 
-// para hacer el los efectos en los input
-$(document).ready(function(){
-    $('input.input__field').each(function() { 
-        var $this = $(this);
-        if($.trim($this.val()) !== '') {
-            $this.parent().addClass('input--filled');
-        }
-
-        $this.on('focus', function(){
-            var $this = $(this);
-            // $this.data('hasValue', $.trim($this.val()) !== '');
-            $this.parent().addClass('input--filled');
-        }).on('blur', function(){
-            var $this = $(this);
-            if($.trim($this.val()) === '') {
-                $this.parent().removeClass('input--filled');
-            }
-        });
-    });
-});
-
-
-
-// autocomplete de direcciones
-$(function(){
-$("#origen").geocomplete()
- .bind("geocode:result", function(event, result){
-$.log("Result: " + result.formatted_address);
+  $('.form-section-2').each(function (index, section) {
+    $(section).find(':input').attr('data-parsley-group', 'block-' + String(index));
+  })
+  
+  // Inicia la navecación.
+  navigateTo(0);
 })
-});
 
-$(function(){
-$("#destino").geocomplete()
- .bind("geocode:result", function(event, result){
-$.log("Result: " + result.formatted_address);
-})
-});
+function navigateTo (index) {
+  var $sections = $('.form-section-2');
+  $sections.removeClass('current')
+    .eq(index)
+    .addClass('current');
 
-
-
-// para mostrar el select de servicio por hora y ocultarlo si se seleciona por destino
-function mostrarDestino() {
-    $(".selehor").css({"display": "none"});
-    $(".seledes").css({"display": "block"});
-
-    $(".servipresudes").css({"background-color": "#4accff"});
-    $(".servipresudes").css({"color": "#fff"});
-
-    $(".servipresuhor").css({"background-color": "rgba(24,91,117,.3)"});
-    $(".servipresuhor").css({"color": "#b5b4b4"});
+  var $formNavigation = $('.form-navigation-2');
+  $formNavigation.find('.previous').toggle(index > 0);
+  var atTheEnd = index >= $sections.length - 1;
+  $formNavigation.find('.next').toggle(!atTheEnd);
+  $formNavigation.find('[type=submit]').toggle(atTheEnd);
 }
 
-function mostrarHora() {
-    $(".servipresudes").css({"background-color": "rgba(24,91,117,.3)"});
-    $(".servipresudes").css({"color": "#b5b4b4"});
-
-    $(".servipresuhor").css({"background-color": "#4accff"});
-    $(".servipresuhor").css({"color": "#fff"});
-
-    $(".seledes").css({"display": "none"});
-    $(".selehor").css({"display": "block"});
+function curIndex () {
+  var $formSection = $('.form-section-2');
+  return $formSection.index($formSection.filter('.current'));  
 }
 
+/**
+ * mostrarDestino
+ */
+function mostrarDestino () {
+  $('.selehor').hide();
+  $('.seledes').show();
+  $('.servipresudes').css({'background-color': '#4accff', 'color': '#fff'});
+  $('.servipresuhor').css({'background-color': 'rgba(24,91,117,.3)', 'color': '#b5b4b4'});
+}
 
-
-// Enviar el nombre del usuario a la instacia 3 y 4 del formulario
-$(document).ready(function()
-	{
-	$("#enviausuario").click(function () {
-	$("#nombreusuarioa").val($("#nombre").val());
-	});		
-});
-$(document).ready(function()
-	{
-	$("#enviausuario").click(function () {
-	$("#nombreusuariob").val($("#nombre").val());
-	});		
-});
-
-
-
-
-
-
+/**
+ * mostrarHora
+ */
+function mostrarHora () {
+  $('.servipresudes').css({'background-color': 'rgba(24,91,117,.3)', 'color': '#b5b4b4'});
+  $('.servipresuhor').css({'background-color': '#4accff', 'color': '#fff'});
+  $('.seledes').hide();
+  $('.selehor').show();
+}
