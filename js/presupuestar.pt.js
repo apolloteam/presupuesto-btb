1d8fb196ab634453ac00942b7289a4e322330b8c
digitalizarte <digitalizarte@gmail.com>
2017-05-19 16:56:47 -0300

    Se agregaron los archivos de idioma.

---
+++ js/presupuestar.pt.js
@@ -0,0 +1,33 @@
+var i18n = {
+	validation: {
+		messages: {
+			nombre: "Ingrese su nombre por favor",
+			apellido: "Ingrese su apellido por favor",
+			email: "Ingrese un mail v\u00E1lido",
+			telefono: "Ingrese su n\u00FAmero telef\u00F3nico",
+			company: "Ingrese el nombre de su compa\u00F1ia",
+			account: "Ingrese un n\u00FAmero v\u00E1lido de cuenta",
+			nombre_pax: "Ingrese un nombre v\u00E1lido por favor",
+			apellido_pax: "Ingrese un apellido v\u00E1lido por favor",
+			fecha: "Deber\u00E1 ingresar una fecha para el traslado",
+			hora: "Deber\u00E1 ingresar una Hora para el traslado",
+			origen: "Deber\u00E1 ingresar un origen para el traslado",
+			destino: "Deber\u00E1 ingresar un destino para el traslado",
+			emergencia: "Ingrese un n\u00FAmero v\u00E1lido por favor"
+		},
+		required: "Este campo es requerido",
+		email: "Ingrese un correo electronico valido",
+		date: "Ingrese una fecha valida",
+		number: "Ingrese un numero valido"
+	},
+	options: {
+		dateFormat: 'd/m/Y',
+		language: 'pt',
+		timeFormat: 'H:i'
+	},
+	messages: {
+		quoteRequestError: "Ocorreu um erro"
+	},
+	successUrl: "http://dottransfers.com/pt/obrigado/",
+	QuantityText : "Quantidade"
+}
